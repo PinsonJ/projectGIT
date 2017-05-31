c1386c46b15973a9021500658b0cef3983f49085
Jonas Pinson <jonas.pinson@gmail.com>
2017-05-31 22:00:55 +0200

    basic loginform, error page. begin usage of materializecss

--- init.py
+++ init.py
@@ -1,13 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for,request, session, flash
 from DbClass import DbClass
+from functools import wraps
 
 app = Flask(__name__)
 
+app.secret_key = "key"
+
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
 
 @app.route('/')
+@login_required
 def index():
 
     return render_template('index.html')
+
+
+@app.route('/login', methods = ['GET','POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid credentials. Please try again'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('logged_in', None)
+    return  redirect(url_for('index'))
 
 # @app.route('/collection')
 # def collection():
@@ -31,6 +63,6 @@ def pagenotfound(error):
     return render_template("error.html", error=error)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
 #
